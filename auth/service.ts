ae501d7fb830efb668c6e0888da11502a3c774a2
9init <emovent2e@gmail.com>
2023-07-28 02:09:54 +0300

    fix error

--- auth/service.ts
+++ auth/service.ts
@@ -8,43 +8,58 @@ import {User, iUser} from "../models/User"
 
 const users: iUser[] = [];
 
-passport.use("stateless", new CustomStrategy((req, done)=>{
+passport.use("stateless", new CustomStrategy((req, done) => {
     // get the email and password that being sent by the client side
     let { email, password } = req.body
 
     const user = users.find(user => user.email == email);
 
-    if (!user) return done({ message: "Incorrect information." }) // return custom error message
+    if (!user) {
+        return done(null, { message: "Incorrect information." }); // Return false to indicate authentication failure and provide custom error message
+    }
 
+    bcrypt.compare(password, user.password, (err: Error, success: Boolean) => {
+        if (err) {
+            return done(err);
+        }
+        if (!success) {
+            return done(null, { message: "Incorrect information." }); // Return false to indicate authentication failure and provide custom error message
+        }
+        return done(null, user); // Correct information
+    });
 
-    bcrypt.compare(password, user.password, (err: Error, success: Boolean)=>{
-        if (err) return done(err) // return error message again
-        if (!success) return done({ message: "Incorrect information." }) // return custom error message again
-        return done(null, user) // Correct information
-    })
-
-}))
+}));
 
 passport.serializeUser<iUser>((user: any, done: (err: any, id: any) => void)=>{
     done(null, user.id)
 })
 
-passport.deserializeUser((id: mongoose.ObjectId, done)=>{
-    const user = users.find(user => user.id == id);
-    done(null, user)
-})
+passport.serializeUser<iUser>((user: Express.User, done: (err: any, id: any) => void) => {
+    // Use the user's id property as the serialized identifier for the session
+    done(null, user);
+});
+
+
+passport.deserializeUser((id: string, done) => {
+    const user = users.find(user => user.id === id);
+    if (!user) {
+        return done(new Error("User not found")); // Return an error if user not found
+    }
+    done(null, user);
+});
 
 async function registerHandler(req: express.Request, res: express.Response) {
     // you should validate them but I won't do this here
     let { name, email, password } = req.body
 
     // Check if email is already used
-    let exist = await User.exists({ email: email })
+    let exist = users.find(user => user.email == email)
     if (exist) return res.status(403).send("Email already used.")
     
     // generate hashed password
     let salt = await bcrypt.genSalt(10)
     let hash = await bcrypt.hash(password, salt)
+
 
     // create user model
     let newUser = new User({
@@ -55,10 +70,13 @@ async function registerHandler(req: express.Request, res: express.Response) {
     })
 
     users.push(newUser)
+    res.send(newUser)
 }
 
 function homeHandler(req: express.Request, res: express.Response) {
-    if(req.isAuthenticated()) res.send(req.user)
+    if(req.isAuthenticated()) {
+        res.send(req.user)
+    }
     else res.send("You are not logged in")    
 }
 
